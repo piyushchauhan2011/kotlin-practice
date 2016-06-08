5667876c2c35cf81b19899d2e861e5b4805efa16
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-06-09 00:34:45 +1000

    Refactored to use data class and implement comparable interface with it for sorting

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,12 +1,9 @@
 /**
  * Created by piyushchauhan on 8/06/2016.
  */
-class Person(val name: String, val age: Int): Comparable<Person>  {
+
+data class Person(val name: String, val age: Int): Comparable<Person> {
     override fun compareTo(other: Person): Int {
         return this.name.compareTo(other.name)
-    }
-
-    override fun toString(): String {
-        return "(${this.name}, ${this.age})"
     }
 }
