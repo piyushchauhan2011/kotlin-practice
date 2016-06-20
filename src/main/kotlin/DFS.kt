a951a8cca06c653d7771846ed962f7ea9248403f
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-06-20 18:46:29 +1000

    Added DFS init and test

---
+++ src/main/kotlin/DFS.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by piyushchauhan on 20/06/2016.
+ */
+data class Edge(val v: Int, val w: Int, val weight: Double)
+
+fun main(args: Array<String>) {
+    val e1 = Edge(2, 3, 23.4)
+    println(e1)
+}
